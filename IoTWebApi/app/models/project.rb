47eb7c12eb32ea4077bb22b02e69fd8d77a7a308
André Farinha <andre_farinha@hotmail.com>
2016-12-19 22:30:19 +0000

    Set discipline -> Project

--- IoTWebApi/app/models/project.rb
+++ IoTWebApi/app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ApplicationRecord
   belongs_to :discipline
+
+#overrides the sets
+	def discipline=(params)
+		@discipline = Discipline.find( params[:id])	
+		super(@discipline)
+	end
+
+	#Overrides the Gets
+	def discipline	
+		super
+ 	end
+
 end
