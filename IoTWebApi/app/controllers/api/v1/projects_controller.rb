1aee56575b7bb5b716934f9264dcd0e95ad9475c
André Farinha <andre_farinha@hotmail.com>
2016-12-29 14:48:10 +0000

    Disciplinas nos projetos

--- IoTWebApi/app/controllers/api/v1/projects_controller.rb
+++ IoTWebApi/app/controllers/api/v1/projects_controller.rb
@@ -47,7 +47,7 @@ module Api::V1
 
       # Only allow a trusted parameter "white list" through.
       def project_params
-        params.require(:project).permit(:name, :description, :grade, :discipline_id)
+        params.require(:project).permit(:name, :description, :grade, :discipline => [:id])
       end
   end
 end
